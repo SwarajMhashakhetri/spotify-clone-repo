3dc4b65f79359421b2ee3527c62a78f40469b3b0
piyushcodes07 <piyushcodes07@gmail.com>
2024-02-01 00:15:36 +0530

    fixed songs not loading

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
+    console.log(currFolder);
     let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
